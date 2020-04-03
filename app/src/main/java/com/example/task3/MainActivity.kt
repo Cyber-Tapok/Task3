b59ffa275965db2a5ccdef10fd471dbfcd48b0d6
Cyber-Tapok <Cyber-Tapok@users.noreply.github.com>
2020-04-03 15:14:06 +0400

    Add internet listener

--- app/src/main/java/com/example/task3/MainActivity.kt
+++ app/src/main/java/com/example/task3/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.task3
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -9,39 +10,50 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.task3.model.GithubIssue
 import com.google.android.material.snackbar.Snackbar
+import com.treebo.internetavailabilitychecker.InternetAvailabilityChecker
+import com.treebo.internetavailabilitychecker.InternetConnectivityListener
 
 
 class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener,
-    RecyclerAdapter.CallDetailInfo {
+    RecyclerAdapter.CallDetailInfo, InternetConnectivityListener {
 
     private val fragmentManager = supportFragmentManager
     private var recyclerAdapter: RecyclerAdapter = RecyclerAdapter(this)
     private lateinit var recyclerView: RecyclerView
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var issueViewModel: IssueViewModel
+    private var recyclerViewState: Parcelable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         issueViewModel = ViewModelProviders.of(this)[IssueViewModel::class.java]
         swipeRefreshLayout = findViewById(R.id.swipe_container)
         swipeRefreshLayout.setOnRefreshListener(this)
         swipeRefreshLayout.setColorSchemeResources(
             R.color.colorPrimary
         )
-        swipeRefreshLayout.post {
-            loadIssue()
-        }
         recyclerAdapter.setHasStableIds(true)
         recyclerView.adapter = recyclerAdapter
+        if (recyclerViewState != null) {
+            recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
+        }
+        InternetAvailabilityChecker.init(this)
+        val mInternetAvailabilityChecker = InternetAvailabilityChecker.getInstance()
+        mInternetAvailabilityChecker.addInternetConnectivityListener(this)
     }
+
 
     override fun onRefresh() {
         issueViewModel.getAllIssue()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        recyclerViewState = recyclerView.layoutManager?.onSaveInstanceState()
+        super.onSaveInstanceState(outState)
+    }
 
     private fun loadIssue() {
         swipeRefreshLayout.isRefreshing = true
@@ -71,4 +83,19 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener,
         bundle.putParcelable(FRAGMENT_BUNDLE_ISSUE_KEY, issue)
         fragment.arguments = bundle
     }
+
+    override fun onInternetConnectivityChanged(isConnected: Boolean) {
+        if (isConnected) {
+            swipeRefreshLayout.post {
+                loadIssue()
+            }
+        } else {
+            Snackbar.make(
+                recyclerView,
+                getString(R.string.internet_connection),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+    }
+
 }
